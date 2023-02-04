605582ea21ee12938bb63b72d825261663abdfd4
Tom Boustedt <tomboustedt@MacBook-Pro-som-tillhor-Tom.local>
2023-02-04 17:55:51 +0100

    Back-end

---
+++ index.js
@@ -0,0 +1,33 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import ReviewsDAO from "./dao/reviewsDAO.js";
+
+
+const MongoClient = mongodb.MongoClient;
+
+const mongo_username = process.env.MONGO_USERNAME
+const mongo_password = process.env.MONGO_PASSWORD
+
+const uri = `mongodb+srv://Tombo:Jultomten123@cluster0.eyjwumf.mongodb.net/?retryWrites=true&w=majority`;
+
+const port = 8000;
+
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true,
+    }
+)
+.catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+})
+.then(async client => {
+    await ReviewsDAO.injectDB(client);
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+})
